fe4a92ed6d22c43cc8efb5d3eaeba232a7381b61
Christian Vinicius <christianrtbard1945@gmail.com>
2025-08-14 16:52:02 -0300

    Add files via upload
    calcular a área de um retângulo. O programa deve solicitar ao usuário que forneça a base e a altura do retângulo. Em seguida, o programa deve calcular a área usando a fórmula.

---
+++ BaseRetangulo.py
@@ -0,0 +1,5 @@
+#Programa para calcular a area do retangulo
+base = float(input("Digite a base: "))
+altura = float(input("Digite a altura: "))
+area = base * altura
+print(f"A área do retângulo é: {area:.2f} metros quadrados")
