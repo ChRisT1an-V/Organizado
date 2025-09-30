7f7353f8576ea76c38b015bd538d84d738995ca5
Christian Vinicius <christianrtbard1945@gmail.com>
2025-09-30 14:56:09 -0300

    Conjunt.py

---
+++ Conjunt.py
@@ -0,0 +1,14 @@
+#Atividade 1 Crie um conjunto vazio chamado frutas e adicione as seguintes frutas a ele: "maçã", "banana", "uva", "laranja" e "morango". Em seguida, imprima o conjunto.
+frutas = set()
+
+frutas.add("maçã")
+frutas.add("banana")
+frutas.add("uva")
+frutas.add("laranja")
+frutas.add("morango")
+
+print(frutas)
+
+#Atividade 2 Crie um conjunto de frutas, Verifique se a fruta  "banana " está presente no conjunto frutas e imprima o resultado.
+frutas = {"maçã", "banana", "uva", "laranja", "morango"}
+print (frutas)
