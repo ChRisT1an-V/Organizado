6fee3a679c72661674571805800456589d55bdf3
Christian Vinicius <christianrtbard1945@gmail.com>
2025-10-01 14:27:15 -0300

    Fist_code.py

--- Fist_code.py
+++ Fist_code.py
@@ -1,4 +1,4 @@
-# Atividade 02: Crie um programa que peça ao usuário para digitar: 1.Seu nome; 2.Sua idade; 3.Sua altura; 4.Em seguida, imprima esses valores e seus respectivos tipos.
+# Atividade: Crie um programa que peça ao usuário para digitar: 1.Seu nome; 2.Sua idade; 3.Sua altura; 4.Em seguida, imprima esses valores e seus respectivos tipos.
 
 nome = str (input("Digite seu nome: "))
 idade = int(input("Digite sua idade: "))
